e0d71a8f077ed120f986a0e59e635054a500f97a
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-11-01 13:08:00 +0800

    da

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const Users = require("./models/userModel");
 const Products = require("./models/productModel");
@@ -11,6 +12,14 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 //default route
 app.get("/", (req, res) => {
