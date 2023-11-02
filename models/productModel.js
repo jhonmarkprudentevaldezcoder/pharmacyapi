ac9e5e0f828a231290671401389fe969636746da
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-11-02 12:24:16 +0800

    da

--- models/productModel.js
+++ models/productModel.js
@@ -4,23 +4,18 @@ const productSchema = mongoose.Schema(
   {
     Name: {
       type: String,
-      required: [true, "Please enter first name"],
     },
     Description: {
       type: String,
-      required: [true, "Please enter last name"],
     },
     Price: {
       type: String,
-      required: [true, "Please enter email"],
     },
     Stock: {
       type: String,
-      required: [true, "Please enter password"],
     },
     Image: {
       type: String,
-      required: [true, "Please enter password"],
     },
   },
   {
