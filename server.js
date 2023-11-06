684a3a43ef6e54b891b5e412367670c4aa042b1c
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-11-06 23:19:56 +0800

    da

--- server.js
+++ server.js
@@ -80,12 +80,21 @@ app.post("/product", async (req, res) => {
   }
 });
 
-//get all thenes
+//get all products
 app.get("/product", async (req, res) => {
   try {
     const products = await Products.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//count products
+app.get("/product/count", async (req, res) => {
+  try {
     const productCount = await Products.countDocuments({});
-    res.status(200).json({ products, totalProducts: productCount });
+    res.status(200).json(productCount);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
