d3c2182feac855298fa170346cdb36c45afbaa78
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-11-18 16:44:38 +0800

    dada

--- models/userModel.js
+++ models/userModel.js
@@ -17,6 +17,10 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please enter email"],
     },
+    contact: {
+      type: String,
+      required: [true, "Please enter contact"],
+    },
     password: {
       type: String,
       required: [true, "Please enter password"],
