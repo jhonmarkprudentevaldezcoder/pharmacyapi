a7ab1fe70610de314024e636c6c4cd3738026546
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-11-19 01:00:55 +0800

    DA

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.put("/user/shipped/:id", async (req, res) => {
 app.get("/order/count", async (req, res) => {
   try {
     const orderCount = await Order.countDocuments({
-      OrderStatus: { $ne: "DELIVERED" },
+      OrderStatus: { $ne: "NO DELIVERED" },
     });
     res.status(200).json(orderCount);
   } catch (error) {
