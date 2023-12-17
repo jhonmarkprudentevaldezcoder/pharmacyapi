29cafeca2e6bc7ffdbb6b1c2942c37c5fcb72da3
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-12-18 00:01:05 +0800

    dada

--- models/productModel.js
+++ models/productModel.js
@@ -10,9 +10,21 @@ const productSchema = mongoose.Schema(
     },
     Price: {
       type: Number,
+      validate: {
+        validator: function (value) {
+          return value >= 0; // Check if the value is greater than or equal to 0
+        },
+        message: "Price must be greater than or equal to 0",
+      },
     },
     Stock: {
       type: Number,
+      validate: {
+        validator: function (value) {
+          return value >= 0; // Check if the value is greater than or equal to 0
+        },
+        message: "Stock must be greater than or equal to 0",
+      },
     },
     Image: {
       type: String,
